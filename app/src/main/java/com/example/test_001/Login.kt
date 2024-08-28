48edb10fd12497a30d782e993cd1bbc79371ae40
35-hasib <hrrahman35@gmail.com>
2024-08-28 18:56:46 +0600

    add Button

--- app/src/main/java/com/example/test_001/Login.kt
+++ app/src/main/java/com/example/test_001/Login.kt
@@ -1,11 +1,22 @@
 package com.example.test_001
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-@Preview
-fun Login(){
-    Text(text = "Hello Kotlin")
+fun Login() {
+
+    Button(onClick = { /*TODO*/ }) {
+        Text(
+            text = "Login",
+            color = Color.Red,
+
+        )
+    }
 }
